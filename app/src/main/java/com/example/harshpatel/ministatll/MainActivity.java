6ff35c7892444297e5aba086773f37cfcae29a05
Harshpatel682 <patel682@purdue.edu>
2017-06-29 15:28:14 -0400

    second commit

---
+++ app/src/main/java/com/example/harshpatel/ministatll/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.harshpatel.ministatll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
