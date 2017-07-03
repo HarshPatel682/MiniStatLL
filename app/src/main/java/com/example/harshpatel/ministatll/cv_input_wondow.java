b11a8975c7e6204c3565557c1badd5e4bc62c785
Harshpatel682 <patel682@purdue.edu>
2017-07-03 16:15:34 -0400

    made CV page, going to connect it to the main activity

---
+++ app/src/main/java/com/example/harshpatel/ministatll/cv_input_wondow.java
@@ -0,0 +1,13 @@
+package com.example.harshpatel.ministatll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cv_input_wondow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cv_input_wondow);
+    }
+}
