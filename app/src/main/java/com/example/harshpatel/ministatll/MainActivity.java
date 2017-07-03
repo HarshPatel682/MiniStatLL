4e0c9f2eb295fc5e8428da385f67281d9edb52da
Harshpatel682 <patel682@purdue.edu>
2017-07-03 16:34:03 -0400

    made CV page, going to connect it to the main activity

--- app/src/main/java/com/example/harshpatel/ministatll/MainActivity.java
+++ app/src/main/java/com/example/harshpatel/ministatll/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.harshpatel.ministatll;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.CharacterPickerDialog;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = (Spinner)findViewById(R.id.dropDownForExperiments);
+        adapter = ArrayAdapter.createFromResource(this,R.array.experiments,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     }
 }
