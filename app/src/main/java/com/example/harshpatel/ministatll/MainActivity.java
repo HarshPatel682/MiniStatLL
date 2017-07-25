98b66dc934e18d5377c299fac7e6764e81eacf1e
Harshpatel682 <patel682@purdue.edu>
2017-07-25 16:20:14 -0400

    Tried to do the bluetooth connectivity, was giving me errors added some comments in the main_activity adjusted all the inputs for the experiment pages so the user can input decimal values
    Next:
    crank out the bluetooth stuff (hopefully understand it)

--- app/src/main/java/com/example/harshpatel/ministatll/MainActivity.java
+++ app/src/main/java/com/example/harshpatel/ministatll/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.harshpatel.ministatll;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,15 +15,31 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
+    public boolean isConnected = false;
+    private BluetoothAdapter BA = BluetoothAdapter.getDefaultAdapter();
+    String deviceAddress;
+    public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
+    public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
+    public final static String EXTRA_DATA =
+            "com.example.bluetooth.le.EXTRA_DATA";
+    public final static String ACTION_DATA_AVAILABLE =
+            "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
+    private BluetoothGattCharacteristic characteristic;
+    //private BluetoothLeService mBluetoothLeService;
 
-    Spinner spinner;
-    ArrayAdapter<CharSequence> adapter;
+
+
+    Spinner spinner; //drop down list
+    ArrayAdapter<CharSequence> adapter;//returns the value from out spinner
     TextView selectedExperiment; //will store the textview of the selected experiment
 
-    public Button selectedExperimentButton;
+    public Button selectedExperimentButton; //this button will send the user to the specified experiment page
 
     public void init() {
         selectedExperimentButton = (Button) findViewById(R.id.selectedExperimentButton);
+        /*when ever the continue button is pressed, this will use the value from the
+        * spinner to go to the specified experiment page
+        */
         selectedExperimentButton.setOnClickListener(new View.OnClickListener() {
             Intent openSelectedExperimentPage;
             @Override
@@ -46,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                         openSelectedExperimentPage = new Intent(MainActivity.this, pv_input_window.class);
                         break;
                 }
-                startActivity(openSelectedExperimentPage);
+                startActivity(openSelectedExperimentPage); //this will send the user to the one of the experiment pages
             }
         });
     }
@@ -55,12 +73,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
+        init(); //sets up the above code onCreate
 
+        /*Initializing the spinner and adapter */
         spinner = (Spinner)findViewById(R.id.dropDownForExperiments);
         adapter = ArrayAdapter.createFromResource(this,R.array.experiments,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        //this will set the "selectedExperiment" to the experiment selected from the dropdown list
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
