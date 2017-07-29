5729f2309fb8612516361e54846eb9dda893ed03
Harshpatel682 <patel682@purdue.edu>
2017-07-29 17:04:24 -0400

    tried to make the graph use the example data, but it kept messing up. I may have to restart the graphing stuff since what i have now doesn't work when trying to override data points...

--- app/src/main/java/com/example/harshpatel/ministatll/graph_example.java
+++ app/src/main/java/com/example/harshpatel/ministatll/graph_example.java
@@ -14,6 +14,7 @@ import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.MarkerImage;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
@@ -34,12 +35,18 @@ import java.text.SimpleDateFormat;
 
 public class graph_example extends AppCompatActivity {
 
+    static ArrayList<Double> exampleX;
+    static ArrayList<Double> exampleY;
+
+
+
     static ArrayList<Double> xValues;
     static ArrayList<Double> yValues;
 
     private LineChart mChart;
     double x = 0;
     double y = 0;
+    int counter = 0;
     private Button stop_button, save_data_onto_file_button;
     private boolean shouldIStop;
 
@@ -53,15 +60,19 @@ public class graph_example extends AppCompatActivity {
         //ASK ORLANDO if he wants the user to be able to name the file...
         String FILENAME = "output.txt"; //it can be changed to .csv after
         String data = "" + date + "\n";
-        for (int i = 0; i < xValues.size(); i++) {
-            if(i == (xValues.size() - 1)) {
-                data += "(" + xValues.get(i) + "," + yValues.get(i) + ")";
-            } else {
-                data += "(" + xValues.get(i) + "," + yValues.get(i) + "),";
-            }
-        }
-        data += "\n";
+//        for (int i = 0; i < xValues.size(); i++) {
+//            if(i == (xValues.size() - 1)) {
+//                data += "(" + xValues.get(i) + "," + yValues.get(i) + ")";
+//            } else {
+//                data += "(" + xValues.get(i) + "," + yValues.get(i) + "),";
+//            }
+//        }
+//        data += "\n";
 
+
+        for (int i = 0; i < xValues.size(); i++) {
+            data += xValues.get(i) + "," + yValues.get(i) + "\n";
+        }
 
         String state;
         state = Environment.getExternalStorageState();
@@ -110,6 +121,38 @@ public class graph_example extends AppCompatActivity {
         * https://www.youtube.com/watch?v=a20EchSQgpw&index=5&t=908s&list=PLVDfjBEFgjhBCpeOW11AupWj0bYV1UWrt
         */
 
+        double[] xs = {-33,-67,-134,-201,-268,-336,-403,-470,-537 ,-604,-672,
+                       -739 ,-806 ,-739 ,-672 ,-604 ,-537 ,-470 ,-403 ,-336,
+                       -268 ,-201 ,-134 ,-67 ,-33 ,0 ,33,67,134 ,201 ,268,
+                        336 ,403 ,470 ,537 ,604 ,672 ,739 ,806 ,739 ,672,
+                        604 ,537 ,470 ,403 ,336 ,268 ,201 ,134 ,67 ,33 ,0,
+                        -33 ,-67 ,-134 ,-201 ,-268 ,-336 ,-403 ,-470 ,-537,
+                        -604 ,-672 ,-739 ,-806,-739 ,-672 ,-604 ,-537 ,-470,
+                        -403 ,-336 ,-268 ,-201 ,-134 ,-67 ,-33, 0 ,33 ,67 ,134,
+                        201 ,268 ,336 ,403 ,470 ,537 ,604 ,672 ,739 ,806 ,739,
+                        672 ,604 ,537 ,470 ,403 ,336 ,268 ,201 ,134 ,67 ,33,0};
+
+        double[] ys = {-2209 ,-2448 ,-2328 ,-2090 ,-2090 ,-2090 ,-2209 ,-2448,
+                     -2567 ,-2567 ,-2448 ,-2328 ,-2328 ,-2328 ,-2209 ,-2209 ,-2209,
+                     -1851 ,-1731-1612 ,-1492 ,-1492 ,-1492 ,-1373 ,-1373 ,-1254,
+                     -1134 ,-537 ,2926 ,8539 ,10928 ,8897 ,6986 ,5912 ,5195 ,4717,
+                     4359 ,4001 ,4001 ,3165 ,2687 ,2448 ,2328 ,2328 ,2209 ,1970,
+                     1492 ,-537 ,-5314 ,-9375, -8420 ,-7345 ,-6389 ,-5792 -5314,
+                     -4478,-4120 ,-3762 ,-3642,-3642 ,-3881 ,-4120 ,-3762 ,-3523 ,
+                     -3403 ,-3165 ,-3045 ,-2926 ,-2687 ,-2328 ,-2090 ,-1970 ,-1970,
+                     -1970 ,-1851 ,-1731 ,-1731 ,-1612,-1373 ,-895 ,2687 ,8420,
+                      10569,8659 ,6628 ,5553 ,4956 ,4478 ,4120 ,3881 ,3762 ,3045,
+                     2567 ,2328 ,2209 ,2090 ,2090 ,1851 ,1373 ,-656 ,-5434 ,-9495,
+                     -8539 ,-7464};
+
+        exampleX = new ArrayList<Double>();
+        exampleY = new ArrayList<Double>();
+
+        for (int i = 0; i < ys.length; i++) {
+            exampleX.add(xs[i]);
+            exampleY.add(ys[i]);
+        }
+
         xValues = new ArrayList<Double>();
         yValues = new ArrayList<Double>();
 
@@ -157,6 +200,9 @@ public class graph_example extends AppCompatActivity {
 
         XAxis xAxis = mChart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM_INSIDE);
+//        xAxis.setAxisMaximum(1000);
+//        xAxis.setAxisMinimum(-1000);
         xAxis.setTextSize(10f);
         xAxis.setTextColor(Color.RED);
         xAxis.setDrawAxisLine(true);
@@ -165,11 +211,11 @@ public class graph_example extends AppCompatActivity {
 
         YAxis yAxis = mChart.getAxisLeft();
         yAxis.setTextSize(12f); // set the text size
-        yAxis.setAxisMinimum(0f); // start at zero
-        yAxis.setAxisMaximum(100f); // the axis maximum is 100
+        yAxis.setAxisMinimum(-100); // start at zero
+        yAxis.setAxisMaximum(100); // the axis maximum is 100
         yAxis.setTextColor(Color.BLACK);
         yAxis.setGranularity(1f); // interval 1
-        yAxis.setLabelCount(6, true); // force 6 labels
+        yAxis.setLabelCount(100, true); // force 6 labels
 
         YAxis yAxis1 = mChart.getAxisRight();
         yAxis1.setEnabled(false);
@@ -182,7 +228,7 @@ public class graph_example extends AppCompatActivity {
 //        new Thread(new Runnable() {
 //            @Override
 //            public void run() {
-//                for (int i = 0; i < 100; i++) {
+//                for (int i = 0; i < exampleY.size(); i++) {
 //                    runOnUiThread(new Runnable() {
 //                        @Override
 //                        public void run() {
@@ -225,6 +271,10 @@ public class graph_example extends AppCompatActivity {
 
         if (data != null) {
             LineDataSet set = (LineDataSet) data.getDataSetByIndex(0);
+
+//            mChart.notifyDataSetChanged();
+//            mChart.setVisibleXRange(-1000,1000);
+
             if (set == null) {
                 set = createSet();
                 data.addDataSet(set);
@@ -232,20 +282,34 @@ public class graph_example extends AppCompatActivity {
 
             //fake x and y values
             x += Math.PI/16;
-//            y = Math.abs(Math.tan(x))*20;
-            y = Math.abs(Math.sin(x))*100;
+//            if(x > 6) {
+//                x -= Math.PI/8;
+//            } else {
+//                x += Math.PI/16;
+//
+//            }
+//            y = Math.abs(Math.sin(x))*100;
+            y = Math.sin(x)*100;
 
-            if(y > 100) {
-                y = 100;
-            }
+//            if(counter > exampleY.size()) {
+//                shouldIStop = true;
+//                return;
+//            }
+//
+//            x = exampleX.get(counter);
+//            y = exampleY.get(counter);
+//            counter++;
             data.addEntry(new Entry((float)x , (float)y), 0);
 
             //Putting the values into the arraylist for X and Y values
             xValues.add(x);
             yValues.add(y);
 
+            data.notifyDataChanged();
             mChart.notifyDataSetChanged();
             mChart.setVisibleXRange(0,6);
+//            mChart.setVisibleXRangeMaximum(2000);
+//            mChart.invalidate();
             mChart.moveViewToX((float) (x));
         }
     }
